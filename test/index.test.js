ca4b1fa5457cb3fff482e2d48081ff3cbbf7d979
Nelson <contact.nelsonic@gmail.com>
2015-10-12 18:04:54 +0100

    adds test for incorrect password

--- test/index.test.js
+++ test/index.test.js
@@ -5,18 +5,36 @@ var file  = dir + __filename.replace(__dirname, '') + ' -> ';
 
 /************************ SETUP HAPI SERVER ***************************/
 var Hapi   = require('hapi');  // https://github.com/nelsonic/learn-hapi
-var server = new Hapi.Server({ debug: false })
+var server = new Hapi.Server(); //{ debug: {"request": ["error", "uncaught"]} });
 server.connection({ port: 8000 });
+
+var Bcrypt = require('bcrypt');
+
+var user = {
+    email: 'john@smith.net',
+    password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
+    name: 'John Doe ',
+    id: '2133d32a'
+};
 
 // define which fields we want to validate for
 var Joi    = require('joi');
 var fields = {
   email     : Joi.string().email().required(),
-  password  : Joi.string().required().min(6) // minimum length 6 characters
+  password  : Joi.string().required().min(5) // minimum length 6 characters
 }
 function handler (request, reply) {
   console.log(request.payload)
-  reply('test passed');
+  Bcrypt.compare(request.payload.password, user.password, function (err, isValid) {
+    // console.log(' - - - - - - - - - - - - - - - - - - ');
+    // console.log(err);
+    // console.log(' - - - - - - - - - - - - - - - - - - ');
+      if(!err && isValid) {
+        reply('success');
+      } else {
+        reply('fail').code(400);
+      }
+  });
 }
 var opts   = { fields:fields, handler:handler };
 
@@ -26,7 +44,7 @@ server.register([{ register: require('../lib'), options:opts }], function (err) 
 });
 
 /************************* TESTS ***************************/
-test(file+'Attempt to submit a registration without password', function(t){
+test(file+'Attempt to submit a /login request without password', function(t){
   var options = {
     method: "POST",
     url: "/login",
@@ -36,32 +54,32 @@ test(file+'Attempt to submit a registration without password', function(t){
   server.inject(options, function(response) {
     // joi returns 400 when auth validation fails.
     var code = response.statusCode
-    t.equal(code, 400, 'Register without password fails -> '+code);
+    t.equal(code, 400, '/login without password fails -> '+code);
     server.stop(function(){ t.end() });
   });
 })
 
-test(file+'Attempt to register with unrecognised field', function(t){
+test(file+'Attempt to /login with unrecognised field', function(t){
   var options = {
     method: "POST",
     url: "/login",
-    payload : { email:'this@here.net', password: 'pass4567', name:'Emit' }
+    payload : { email:'this@here.net', password: 'pass4567', job:'Magician' }
   };
 
   server.inject(options, function(response) {
     // joi returns 400 when auth validation fails.
     var code = response.statusCode
-    t.equal(code, 400, 'Register with unknown field fails -> '+code);
+    t.equal(code, 400, '/login with unknown field fails -> '+code);
     server.stop(function(){ t.end() });
   });
 })
 
-var person = {
-  "email" : 'dwyl.test+login-payload' +Math.random()+'@gmail.com',
-  "password":"EverythingIsAwesome"
-}
 
-test(file+"Successfully register with email and password", function(t) {
+test(file+"Successfully /login with email and password", function(t) {
+  var person = {
+    "email" : 'john@smith.net',
+    "password":'secret'
+  }
   var options = {
     method: "POST",
     url: "/login",
@@ -70,7 +88,25 @@ test(file+"Successfully register with email and password", function(t) {
 
   server.inject(options, function(response) {
     // console.log(response)
-    t.equal(response.statusCode, 200, "Register worked with email and password");
+    t.equal(response.statusCode, 200, "/login worked with email and password");
+    server.stop(function(){ t.end() });
+  });
+});
+
+test(file+"Subit /login with incorrect password", function(t) {
+  var person = {
+    "email"    : 'john@smith.net',
+    "password" : 'incorrect'
+  }
+  var options = {
+    method: "POST",
+    url: "/login",
+    payload : person
+  };
+
+  server.inject(options, function(response) {
+    // console.log(response)
+    t.equal(response.statusCode, 400, "/login faild when  password incorrect");
     server.stop(function(){ t.end() });
   });
 });
